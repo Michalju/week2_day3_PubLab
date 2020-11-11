ee32298300ebb8c9e5b1bead79c38f27bdb088dd
Kirsten Nicholson <kirstennic6@gmail.com>
2020-11-11 14:46:42 +0000

    added sell the drinks

--- src/pub.py
+++ src/pub.py
@@ -1,5 +1,27 @@
 class Pub:
-   def __init__(self, name, till, drinks):
+    def __init__(self, name, till, drinks):
         self.name = name
         self.till = till
         self.drinks = drinks
+
+    def drink_exists(self, drink_name):
+        for drink in self.drinks:
+            if drink.name == drink_name:
+                return True 
+        return False
+
+    def cost_of_drink(self, drink_name):
+        for drink in self.drinks:
+            if drink.name == drink_name:
+                return drink.price
+            
+
+    def sell_the_drink(self, drink_name, customer):
+        #check the drink exists
+        #if drink exists - can customer afford it 
+        if self.drink_exists(drink_name) and self.cost_of_drink(drink_name) <= customer.wallet:
+        #reduce customer wallet 
+            customer.wallet -= self.cost_of_drink(drink_name)
+        # increase pub cash/total
+            self.till += self.cost_of_drink(drink_name)
+        
--- tests/pub_test.py
+++ tests/pub_test.py
@@ -1,6 +1,7 @@
 import unittest
 from src.pub import Pub
 from src.drink import Drink
+from src.customer import Customer
 
 class TestPub(unittest.TestCase):
     def setUp(self):
@@ -8,7 +9,8 @@ class TestPub(unittest.TestCase):
         self.drink_2 = Drink("Gin", 15)
         self.drink_3 = Drink("Whisky", 20)
         self.BrewDog = Pub("Brewdog_edinburgh", 100, [self.drink_1, self.drink_2, self.drink_3])
-
+        self.customer = Customer("Billy", 200)
+        self.customer_2 = Customer("Jane", 5)
 
     #@unittest.skip("Delete this line to run the test")
     def test_create_first_pub(self):
@@ -16,4 +18,23 @@ class TestPub(unittest.TestCase):
         self.assertEqual(self.BrewDog.till, 100)
         self.assertNotEqual(self.BrewDog.till, 200)
         self.assertEqual(len(self.BrewDog.drinks), 3)
+        
+
+    def test_drink_exists(self):
+        self.assertTrue(self.BrewDog.drink_exists("Guinness"))
+        self.assertTrue(self.BrewDog.drink_exists("Gin"))
+        self.assertTrue(self.BrewDog.drink_exists("Whisky"))
+        self.assertFalse(self.BrewDog.drink_exists("Wine"))
+
+    def test_sell_the_drink(self):
+        self.BrewDog.sell_the_drink("Gin", self.customer)
+        self.assertEqual(self.customer.wallet, 185) 
+        self.assertEqual(self.BrewDog.till, 115)
+        self.BrewDog.sell_the_drink("Wine", self.customer)
+        self.assertEqual(self.customer.wallet, 185) 
+        self.assertEqual(self.BrewDog.till, 115)
+        self.BrewDog.sell_the_drink("Gin", self.customer_2)
+        self.assertEqual(self.customer_2.wallet, 5) 
+        self.assertEqual(self.BrewDog.till, 115)
+
         
